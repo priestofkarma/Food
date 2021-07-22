2824f59ad9e9c3155e643cc4b236130750f4a90b
Zhenya Petrenko <priestofkarma@gmail.com>
2021-07-22 15:00:28 +0300

    prettify timer code

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -1,6 +1,6 @@
 function timer(id, deadline) {
-    /* timer */
 
+    /* timer */
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
             days = Math.floor((t / (1000 * 60 * 60 * 24))),
